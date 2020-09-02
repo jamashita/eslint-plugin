e48bd4b5dc85d3ac74b5d58224ba737aff762e48
jamashita <jamashita@arachnid.xyz>
2020-09-02 09:50:26 +0900

    react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   'root': true,
   'plugins': [
+    'react',
     'jsx-a11y'
   ],
   'parser': '@typescript-eslint/parser',
@@ -632,6 +633,7 @@ module.exports = {
     ],
     '@typescript-eslint/unbound-method': 'error',
     '@typescript-eslint/unified-signatures': 'off',
+    'import/no-unresolved': 'off',
     'react/boolean-prop-naming': 'off',
     'react/button-has-type': 'error',
     'react/default-props-match-prop-types': 'error',
@@ -640,9 +642,17 @@ module.exports = {
     'react/forbid-component-props': 'off',
     'react/forbid-dom-props': 'off',
     'react/forbid-elements': 'off',
-    'react/forbid-prop-types': 'off',
     'react/forbid-foreign-prop-types': 'error',
+    'react/forbid-prop-types': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        'namedComponents': 'arrow-function',
+        'unnamedComponents': 'arrow-function'
+      }
+    ],
     'react/no-access-state-in-setstate': 'error',
+    'react/no-adjacent-inline-elements': 'error',
     'react/no-array-index-key': 'error',
     'react/no-children-prop': 'error',
     'react/no-danger': 'error',
@@ -656,10 +666,10 @@ module.exports = {
     'react/no-multi-comp': 'error',
     'react/no-redundant-should-component-update': 'error',
     'react/no-render-return-value': 'error',
-    'react/no-set-state': 'off',
-    'react/no-typos': 'error',
+    'react/no-set-state': 'error',
     'react/no-string-refs': 'error',
     'react/no-this-in-sfc': 'error',
+    'react/no-typos': 'error',
     'react/no-unescaped-entities': 'error',
     'react/no-unknown-property': 'error',
     'react/no-unsafe': 'error',
@@ -690,8 +700,8 @@ module.exports = {
         'order': [
           'static-methods',
           'lifecycle',
-          'everything-else',
-          'render'
+          'render',
+          'everything-else'
         ]
       }
     ],
@@ -707,9 +717,22 @@ module.exports = {
       'error',
       'always'
     ],
-    'react/jsx-child-element-spacing': 'off',
-    'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-child-element-spacing': 'error',
+    'react/jsx-closing-bracket-location': [
+      'error',
+      {
+        'nonEmpty': 'tag-aligned',
+        'selfClosing': 'tag-aligned'
+      }
+    ],
     'react/jsx-closing-tag-location': 'error',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {
+        'props': 'never',
+        'children': 'never'
+      }
+    ],
     'react/jsx-curly-newline': [
       'error',
       {
@@ -727,10 +750,14 @@ module.exports = {
       'error',
       'never'
     ],
-    'react/jsx-filename-extension': 'off',
+    'react/jsx-filename-extension': 'error',
     'react/jsx-first-prop-new-line': [
       'error',
       'multiline'
+    ],
+    'react/jsx-fragments': [
+      'error',
+      'syntax'
     ],
     'react/jsx-handler-names': 'error',
     'react/jsx-indent': [
@@ -744,8 +771,18 @@ module.exports = {
       'error',
       2
     ],
-    'react/jsx-key': 'error',
-    'react/jsx-max-depth': 'off',
+    'react/jsx-key': [
+      'error',
+      {
+        'checkFragmentShorthand': true
+      }
+    ],
+    'react/jsx-max-depth': [
+      'error',
+      {
+        'max': 5
+      }
+    ],
     'react/jsx-max-props-per-line': [
       'error',
       {
@@ -761,6 +798,7 @@ module.exports = {
     'react/jsx-no-comment-textnodes': 'error',
     'react/jsx-no-duplicate-props': 'error',
     'react/jsx-no-literals': 'off',
+    'react/jsx-no-script-url': 'error',
     'react/jsx-no-target-blank': [
       'error',
       {
@@ -768,22 +806,12 @@ module.exports = {
       }
     ],
     'react/jsx-no-undef': 'error',
+    'react/jsx-no-useless-fragment': 'error',
     'react/jsx-one-expression-per-line': [
       'error',
       {
         'allow': 'single-child'
       }
-    ],
-    'react/jsx-curly-brace-presence': [
-      'error',
-      {
-        'props': 'never',
-        'children': 'never'
-      }
-    ],
-    'react/jsx-fragments': [
-      'error',
-      'syntax'
     ],
     'react/jsx-pascal-case': 'error',
     'react/jsx-props-no-multi-spaces': 'error',
@@ -813,7 +841,63 @@ module.exports = {
         'prop': 'parens-new-line'
       }
     ],
-    'import/no-unresolved': 'off',
+    'jsx-a11y/alt-text': 'error',
+    'jsx-a11y/anchor-has-content': 'error',
+    'jsx-a11y/anchor-is-valid': 'error',
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'off',
+    'jsx-a11y/aria-props': 'error',
+    'jsx-a11y/aria-proptypes': 'error',
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/aria-unsupported-elements': 'error',
+    'jsx-a11y/autocomplete-valid': 'error',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/heading-has-content': 'error',
+    'jsx-a11y/html-has-lang': 'off',
+    'jsx-a11y/iframe-has-title': 'error',
+    'jsx-a11y/img-redundant-alt': 'off',
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/lang': 'off',
+    'jsx-a11y/media-has-caption': 'error',
+    'jsx-a11y/mouse-events-have-key-events': 'error',
+    'jsx-a11y/no-access-key': 'error',
+    'jsx-a11y/no-autofocus': 'error',
+    'jsx-a11y/no-distracting-elements': 'error',
+    'jsx-a11y/no-interactive-element-to-noninteractive-role': 'error',
+    'jsx-a11y/no-noninteractive-element-interactions': [
+      'error',
+      {
+        handlers: [
+          'onClick',
+          'onMouseDown',
+          'onMouseUp',
+          'onKeyPress',
+          'onKeyDown',
+          'onKeyUp'
+        ]
+      }
+    ],
+    'jsx-a11y/no-noninteractive-element-to-interactive-role': 'error',
+    'jsx-a11y/no-noninteractive-tabindex': 'off',
+    'jsx-a11y/no-onchange': 'error',
+    'jsx-a11y/no-redundant-roles': 'error',
+    'jsx-a11y/no-static-element-interactions': [
+      'error',
+      {
+        handlers: [
+          'onClick',
+          'onMouseDown',
+          'onMouseUp',
+          'onKeyPress',
+          'onKeyDown',
+          'onKeyUp'
+        ]
+      }
+    ],
+    'jsx-a11y/role-has-required-aria-props': 'error',
+    'jsx-a11y/role-supports-aria-props': 'error',
+    'jsx-a11y/scope': 'error',
+    'jsx-a11y/tabindex-no-positive': 'error',
     'jest/no-jasmine-globals': 'off',
     'jest/no-test-callback': 'off'
   }
