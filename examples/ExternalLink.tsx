a2796da2f1250079072a5a661792add0be5bbdc2
jamashita <jamashita@arachnid.xyz>
2020-10-31 01:05:17 +0900

    refactor: 💡 Functionaol Component

--- examples/ExternalLink.tsx
+++ examples/ExternalLink.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 type Props = Readonly<{
   href: string;
@@ -6,36 +6,11 @@ type Props = Readonly<{
     [key: string]: string;
   };
 }>;
-type State = Readonly<{}>;
 
-export class ExternalLink extends React.Component<Props, State> {
-  public shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
-    const {
-      href,
-      style
-    } = this.props;
-
-    if (href !== nextProps.href) {
-      return true;
-    }
-    if (style !== nextProps.style) {
-      return true;
-    }
-
-    return false;
-  }
-
-  public render(): React.ReactNode {
-    const {
-      href,
-      style,
-      children
-    } = this.props;
-
-    return (
-      <a style={style} href={href} target='_blank' rel='noopener noreferrer'>
-        {children}
-      </a>
-    );
-  }
-}
+export const ExternalLink: FC<Props> = ({ href, style, children }) => {
+  return (
+    <a style={style} href={href} target='_blank' rel='noopener noreferrer'>
+      {children}
+    </a>
+  );
+};
