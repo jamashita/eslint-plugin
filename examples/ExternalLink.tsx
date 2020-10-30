2ce9d48ac8d009af4f87cf7bc2d84c149ecbd395
jamashita <jamashita@arachnid.xyz>
2020-10-31 01:14:04 +0900

    test: 💍 Record

--- examples/ExternalLink.tsx
+++ examples/ExternalLink.tsx
@@ -2,9 +2,7 @@ import React, { FC, PropsWithChildren, ReactElement } from 'react';
 
 type Props = Readonly<{
   href: string;
-  style?: {
-    [key: string]: string;
-  };
+  style?: Record<string, string>;
 }>;
 
 export const ExternalLink: FC<Props> = ({ href, style, children }: PropsWithChildren<Props>): ReactElement => {
