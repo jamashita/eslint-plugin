82fbde183aa206b7e162657adb5588546c6a0fcf
jamashita <jamashita@arachnid.xyz>
2021-01-02 20:35:09 +0900

    feat: 🎸 eslint-plugin-node imported and configured

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:import/typescript'
+    'plugin:import/typescript',
+    'plugin:node/recommended'
   ],
   'globals': {
     'NodeJS': false
@@ -730,6 +731,71 @@ module.exports = {
     'import/no-named-export': 'off',
     'import/no-anonymous-default-export': 'error',
     'import/group-exports': 'off',
-    'import/dynamic-import-chunkname': 'off'
+    'import/dynamic-import-chunkname': 'off',
+    /* node */
+    // Possible Errors
+    'node/handle-callback-err': 'off',
+    'node/no-callback-literal': 'off',
+    'node/no-exports-assign': 'off',
+    'node/no-extraneous-import': 'off',
+    'node/no-extraneous-require': 'off',
+    'node/no-missing-import': 'error',
+    'node/no-missing-require': 'error',
+    'node/no-new-require': 'error',
+    'node/no-path-concat': 'error',
+    'node/no-process-exit': 'error',
+    'node/no-unpublished-bin': 'error',
+    'node/no-unpublished-import': 'error',
+    'node/no-unpublished-require': 'error',
+    'node/no-unsupported-features/es-builtins': 'error',
+    'node/no-unsupported-features/es-syntax': 'error',
+    'node/no-unsupported-features/node-builtins': 'error',
+    'node/process-exit-as-throw': 'error',
+    'node/shebang': 'off',
+    // Best Practices
+    'node/no-deprecated-api': 'error',
+    // Stylistic Issues
+    'node/callback-return': 'off',
+    'node/exports-style': [
+      'error',
+      'module.exports'
+    ],
+    'node/file-extension-in-import': 'off',
+    'node/global-require': 'error',
+    'node/no-mixed-requires': 'off',
+    'node/no-process-env': 'off',
+    'node/no-restricted-import': 'off',
+    'node/no-restricted-require': 'off',
+    'node/no-sync': 'error',
+    'node/prefer-global/buffer': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/console': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/process': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/text-decoder': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/text-encoder': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/url-search-params': [
+      'error',
+      'always'
+    ],
+    'node/prefer-global/url': [
+      'error',
+      'always'
+    ],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error'
   }
 };
