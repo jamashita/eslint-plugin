0449ace746554b8bd9967358694a888e4697a0d5
jamashita <jamashita@arachnid.xyz>
2021-07-22 18:18:40 +0900

    feat: 🎸 recommended and default

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,22 +1,5 @@
-'use strict';
-
-module.exports = {
-  'extends': [
-    require.resolve('./es.js'),
-    require.resolve('./import.js'),
-    require.resolve('./jest.js'),
-    require.resolve('./jsx.js'),
-    require.resolve('./node.js'),
-    require.resolve('./react.js'),
-    require.resolve('./ts.js')
-  ],
+export const recommended = {
   'plugins': [
-    'import',
-    'jsx-a11y',
-    'react',
-    'react-hooks',
-    'jest',
-    '@typescript-eslint',
     '@jamashita'
   ]
 };
