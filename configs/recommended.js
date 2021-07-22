4a68796e708d6ade8dbfd147c2fddd4b1d02664c
jamashita <jamashita@arachnid.xyz>
2021-07-22 20:04:46 +0900

    feat: 🎸 abandon ESM

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,7 +1,22 @@
 'use strict';
 
 module.exports = {
+  'extends': [
+    require.resolve('./es.js'),
+    require.resolve('./import.js'),
+    require.resolve('./jest.js'),
+    require.resolve('./jsx.js'),
+    require.resolve('./node.js'),
+    require.resolve('./react.js'),
+    require.resolve('./ts.js')
+  ],
   'plugins': [
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    'jest',
+    '@typescript-eslint',
     '@jamashita'
   ]
 };
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { es } from './configs/es.js';
-import { im } from './configs/import.js';
-import { jest } from './configs/jest.js';
-import { jsx } from './configs/jsx.js';
-import { node } from './configs/node.js';
-import { react } from './configs/react';
-import { recommended } from './configs/recommended.js';
-import { ts } from './configs/ts.js';
+const es = require('./configs/es');
+const im = require('./configs/import');
+const jest = require('./configs/jest');
+const jsx = require('./configs/jsx');
+const node = require('./configs/node');
+const react = require('./configs/react');
+const ts = require('./configs/ts');
+const recommended = require('./configs/recommended');
 
-export default {
+module.exports = {
   'configs': {
     es,
     import: im,
